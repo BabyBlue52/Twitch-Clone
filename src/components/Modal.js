96fb4498c7a63b08195d37c54b60c080c7afd9f0
Chris Hennemann <chris.hennemann256@gmail.com>
2020-03-29 15:53:09 -0400

    Added Delete modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,2 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+const Modal = props => {
+    return ReactDOM.createPortal (
+        <div
+        onClick= {props.onDismiss}
+        className= "ui dimmer modals visible active"
+        >
+            <div 
+            onClick{ ...e => e.stopPropigation()} // the '...' is requied for the eventHandler
+            className= "ui standeard modal visible active">
+                <div className=" header">{props.title}</div>
+                <div className="content">{props.content}</div>
+                <div className="actions">{props.actions}</div>
+            </div>
+        </div>,
+        document.querySelector('#modal')
+    );
+};
+
+export default Modal;
